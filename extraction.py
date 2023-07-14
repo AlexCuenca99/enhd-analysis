6db5d2a594fc89f47b7bba00e2d584c4a20c142d
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-14 11:19:24 -0500

    Extract data from CSV

--- extraction.py
+++ extraction.py
@@ -1,17 +1,18 @@
+# Main imports
 from main import spark
 
-# data_csv = (
-#     spark.read.format("csv")
-#     .option("header", "true")
-#     .option("inferSchema", "true")
-#     .option("delimiter", ";")
-#     .option("enconding", "latin1")
-#     .load("./data/*.csv")
-# )
-# data_csv.write.parquet("./data/enhd_mintel.parquet")
 
-data_file = "./data/mintel_enhd_2021-da.csv"
-sdfData = spark.read.csv(data_file, header=True, sep=";")
-print("Total Records = {}".format(sdfData.count()))
-sdfData.show(5, truncate=False)
-sdfData.printSchema()
+# CSV Pathname
+data_file = "./data/Estudiantes.csv"
+
+# CSV read
+init_dataframe = spark.read.csv(data_file, header=True)
+
+# CSV rows
+print("Total Records = {}".format(init_dataframe.count()))
+
+# Showing first 20 rows
+init_dataframe.show(5, truncate=False)
+
+# CSV schema
+init_dataframe.printSchema()
