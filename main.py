6e160ada1d1d8993f7ba26723b3febede3cbedf0
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-14 11:18:20 -0500

    Add TODOS to main ap

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+# TODO: Quitar el guión en el campo srtCedula [X]
+# TODO: Capitalizar 3 columnas strNombres, srtApellidos y srtNacionalidad [X]
+# TODO: Las otras columnas deben ser eliminadas del dataset nuevo. [X]
+
+# PySpark imports
 import findspark
 from pyspark.sql import SparkSession
-import pyspark.sql.functions as F
 
+
+# Initialize Spark Session
 findspark.init()
 
+# Set Spark Session
 spark = (
+    # Session name
     SparkSession.builder.appName("EncuestaHabilidadesDigitalesMintel")
+    # How many PC cores Spark will use for the session
     .config("spark.sql.shuffle.partitions", "4")
+    # Create session or get if it is already created
     .getOrCreate()
 )
